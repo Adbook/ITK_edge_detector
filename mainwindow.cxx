64533f999923ed3f125ca7f16c6b810ecb5150cf
Adrien Boucaud <adrienboucaud.cpe+git@gmail.com>
2017-07-19 15:13:24 -0400

    Added filter interactions

--- mainwindow.cxx
+++ mainwindow.cxx
@@ -1,23 +1,41 @@
 #include "mainwindow.hxx"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget * parent) : QMainWindow(parent), ui(new Ui::MainWindow), m_input_set(true), m_output_set(false)
 {
 	ui->setupUi(this);
 	
+	ui->button_output->setEnabled(false);
+	ui->button_save->setEnabled(false);
+	ui->sb_upperthresh->setEnabled(false);
+	ui->sb_lowerthresh->setEnabled(false);
+
+	ui->input_label->setText("No input picture chosen");
 	//buttons
 	connect(ui->button_input, SIGNAL(clicked(bool)), this, SLOT(choose_input_file()));
 	connect(ui->button_output, SIGNAL(clicked(bool)), this, SLOT(choose_output_file()));
-
+	connect(ui->button_save, SIGNAL(clicked(bool)), this, SLOT(save_output()));
 	//spinboxes (number inputs)
 	connect(ui->sb_variance, SIGNAL(valueChanged(double)), this, SLOT(update_variance()));
 	connect(ui->sb_upperthresh, SIGNAL(valueChanged(double)), this, SLOT(update_upper_thresh()));
 	connect(ui->sb_lowerthresh, SIGNAL(valueChanged(double)), this, SLOT(update_lower_thresh()));
-
 	//checkbox
 	connect(ui->checkbox_autothreshold, SIGNAL(stateChanged(int)), this, SLOT(toggle_auto_threshold()));
 
+
+	//itk Object creation
+	m_reader = ImageReaderType::New();
+	m_writer = ImageWriterType::New();
+	m_cast_filter = CastFilterType::New();
 	
+	m_filter = FilterType::New();
+	m_filter->SetVariance(ui->sb_variance->value());
+	m_filter->SetAutomaticThresholdingOn();
+
+	m_filter->SetInput(m_reader->GetOutput());
+	m_cast_filter->SetInput(m_filter->GetOutput());
+	m_writer->SetInput(m_cast_filter->GetOutput());
+
 }
 
 
@@ -34,6 +52,9 @@ void MainWindow::choose_input_file()
 
     QPixmap pic (filename);
     ui->input_label->setPixmap(pic);
+    m_reader->SetFileName(filename.toStdString());
+    ui->button_output->setEnabled(true);
+    m_input_set = true;
 }
 
 void MainWindow::choose_output_file()
@@ -41,24 +62,55 @@ void MainWindow::choose_output_file()
 	QString filename = QFileDialog::getSaveFileName(this, tr("Save File"),
                            "~/untitled.png");
 	std::cout << filename.toStdString() << std::endl;
+	m_writer->SetFileName(filename.toStdString());
+	ui->button_save->setEnabled(true);
+	m_output_set = true;
+}
+
+void MainWindow::save_output()
+{
+	if(!m_output_set) return;
+	try {
+	        m_writer->Update();
+	    }catch(itk::ExceptionObject & err)
+	    {
+	        std::cerr << "Exception Caught while writing\n";
+	        std::cerr << err << std::endl;
+	    }
 }
 
 void MainWindow::update_variance()
 {
 	std::cout << "variance "  << ui->sb_variance->value() << std::endl;
+	m_filter->SetVariance(ui->sb_variance->value());
+	save_output();
 }
 
 void MainWindow::toggle_auto_threshold()
 {
 	std::cout << "checkbox " << ui->checkbox_autothreshold->isChecked() << std::endl;
+	if(ui->checkbox_autothreshold->isChecked()){
+		ui->sb_upperthresh->setEnabled(false);
+		ui->sb_lowerthresh->setEnabled(false);
+		m_filter->SetAutomaticThresholdingOn();
+	}else{
+		ui->sb_upperthresh->setEnabled(true);
+		ui->sb_lowerthresh->setEnabled(true);
+		m_filter->SetAutomaticThresholdingOff();
+	}
+	save_output();
 }
 
 void MainWindow::update_upper_thresh()
 {
 	std::cout << "upper thresh " << ui->sb_upperthresh->value() << std::endl;
+	m_filter->SetUpperThreshold(ui->sb_upperthresh->value());
+	save_output();
 }
 
 void MainWindow::update_lower_thresh()
 {
 	std::cout << "lower thresh " << ui->sb_lowerthresh->value() << std::endl;
+	m_filter->SetLowerThreshold(ui->sb_lowerthresh->value());
+	save_output();
 }
--- mainwindow.hxx
+++ mainwindow.hxx
@@ -5,6 +5,15 @@
 #include <QFileDialog>
 #include <QString>
 #include <iostream>
+
+
+#include "itkImage.h"
+#include <itkImageFileWriter.h>
+#include <itkImportImageFilter.h>
+#include <itkImageFileReader.h>
+#include <itkCastImageFilter.h>
+#include "edge_detection_filter.h"
+
 //#include 
 namespace Ui{
 	class MainWindow;
@@ -20,12 +29,34 @@ public:
 protected slots:
 	void choose_input_file();
 	void choose_output_file();
+	void save_output();
 	void update_variance();
 	void toggle_auto_threshold();
 	void update_upper_thresh();
 	void update_lower_thresh();
 private:
 	Ui::MainWindow *ui;
+
+	//todo: controller class ?
+	//itk type definitions
+	typedef float InputPixelType; 
+    typedef unsigned char OutputPixelType;
+    typedef itk::Image<InputPixelType , 2> InputImageType;
+    typedef itk::Image<OutputPixelType , 2> OutputImageType;
+    typedef itk::ImageFileReader<InputImageType> ImageReaderType;
+    typedef itk::ImageFileWriter<OutputImageType> ImageWriterType;
+    typedef EdgeDetectionFilter<InputImageType> FilterType;
+    typedef itk::CastImageFilter<   itk::Image<InputPixelType,2>,
+                                    itk::Image<OutputPixelType,2> >
+                                    CastFilterType;
+
+    ImageReaderType::Pointer m_reader;
+    ImageWriterType::Pointer m_writer;
+    CastFilterType::Pointer m_cast_filter;
+    FilterType::Pointer m_filter;
+
+    bool m_input_set;
+    bool m_output_set;
 
 
 };
